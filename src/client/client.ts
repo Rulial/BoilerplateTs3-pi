808944fa27e2cfcf5643bf78c58b7d299ede39ad
Rulial <98750732+Rulial@users.noreply.github.com>
2022-11-30 08:58:45 +0000

    update

--- src/client/client.ts
+++ src/client/client.ts
@@ -4,7 +4,7 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 const scene = new THREE.Scene()
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.z = 20
+camera.position.z = 90
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
